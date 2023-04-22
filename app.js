4f597818f7e52b4ba4059ffb8c581de232203139
Jose Zangarini <jzangarinim@gmail.com>
2023-04-22 00:27:05 -0400

    added input fields to specify card dimensions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ window.onload = function () {
   bodyStyles();
   stampStyles();
   buttonStyles();
+  inputs();
 };
 
 let cont = document.querySelector(".container");
@@ -30,11 +31,13 @@ cont.insertBefore(value, stampTwo);
 function cardStyles() {
   // Card styles
   cont.style.height = "60vh";
+  //cont.style.minHeight = "270px";
+  //cont.style.minWidth = "100px";
+  cont.style.width = "250px";
   cont.style.background = "white";
   cont.style.border = "1px solid black";
   cont.style.borderRadius = "10px";
-  cont.style.width = "40vh";
-  cont.style.margin = "10vh auto auto auto";
+  cont.style.margin = "5vh auto auto auto";
   cont.style.display = "flex";
   cont.style.flexDirection = "column";
   cont.style.justifyContent = "space-between";
@@ -86,11 +89,10 @@ buttonDiv.appendChild(button);
 
 function buttonStyles() {
   // Button styles
-  let btn = document.querySelector(".btn");
   let div = document.querySelector(".div");
   div.style.display = "flex";
   div.style.justifyContent = "center";
-  div.style.margin = "10vh 0 0 0";
+  div.style.margin = "5vh 0 0 0";
   div.style.height = "5vh";
   div.style.borderRadius = "10px";
 }
@@ -116,6 +118,53 @@ document.addEventListener("click", (event) => {
   if (event.target.classList.contains("btn")) {
     resetCard();
   }
+  // Reset interval timer
   clearInterval(myTimer);
   myTimer = setInterval(interval, 10000);
+});
+
+// Width/height input fields
+// Div
+let dimensions = document.createElement("div");
+dimensions.classList.add("dimensions");
+cont.parentNode.insertBefore(dimensions, cont);
+
+let widthInput = document.createElement("input");
+widthInput.type = "text";
+widthInput.classList.add("width-input");
+dimensions.appendChild(widthInput);
+
+let heightInput = document.createElement("input");
+heightInput.type = "text";
+heightInput.classList.add("height-input");
+dimensions.appendChild(heightInput);
+
+function inputs() {
+  // Inputs styles
+  let dims = document.querySelector(".dimensions");
+  let width = document.querySelector(".width-input");
+  let height = document.querySelector(".height-input");
+
+  // Div styles
+  dims.style.display = "flex";
+  dims.style.justifyContent = "center";
+  dims.style.margin = "10vh 0 0 0";
+
+  // Width styles
+  width.placeholder = "Specify a width (px)";
+  width.style.height = "5vh";
+  width.style.marginRight = "10px";
+
+  // Height styles
+  height.placeholder = "Specify a height (px)";
+  height.style.height = "5vh";
+  height.style.marginLeft = "10px";
+}
+
+document.querySelector(".width-input").addEventListener("change", (event) => {
+  cont.style.width = `${Number(event.target.value)}px`;
+});
+
+document.querySelector(".height-input").addEventListener("change", (event) => {
+  cont.style.height = `${Number(event.target.value)}px`;
 });
